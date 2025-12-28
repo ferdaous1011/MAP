121d1ccc85b35cb9d135d3073e39a175a08760bc
hp <fghalm06@gmail.com>
2025-12-28 15:10:23 +0100

    map and player classes

--- src/game/Player/Player.java
+++ src/game/Player/Player.java
@@ -48,6 +48,9 @@ public class Player {
     public ArrayList<unit> getUnits() {
         return units;
     }
+    public boolean isDead(){
+        return units.isEmpty();
+    }
     //resource
 
 
